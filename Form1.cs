0ad332287c186e6580d061c63c13d5780768fe61
e.guarino@goreply.io <eugenio.guarino@students.plymouth.ac.uk>
2022-04-29 20:30:06 +0200

    feat(ui): visual update and more validation

--- Form1.cs
+++ Form1.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Web;
 
 public class Form1 : Form
 {
@@ -13,8 +13,8 @@ public class Form1 : Form
 
     public void FormLayout()
     {
-        this.Name = "Aria";
-        this.Text = "Aria";
+        this.Name = "Aria Server Launcher";
+        this.Text = "Aria Server Launcher";
         this.Size = new System.Drawing.Size(350, 400);
 
         // avoid resizing
@@ -22,15 +22,17 @@ public class Form1 : Form
         this.FormBorderStyle = FormBorderStyle.FixedSingle;
         this.MaximizeBox = false;
         this.MinimizeBox = false;
+        
 
         // create snarkyPhraseLabel to
         Label titleLabel = new Label();
-        titleLabel.Text = "Aria Server Launcher";
+        titleLabel.Text = "ARIA";
         titleLabel.Location = new Point(70, 10);
-        titleLabel.Font = new Font("Consolas", 12);
+        titleLabel.Font = new Font("Wide Latin", 12);
         titleLabel.TextAlign = ContentAlignment.MiddleCenter;
         titleLabel.Size = new Size(200, 60);
         this.Controls.Add(titleLabel);
+        this.ActiveControl = titleLabel;
 
         // get a random phrase from database
         string randomPhrase = RandomPhrase(PhrasesList.Phrases);
@@ -46,26 +48,28 @@ public class Form1 : Form
 
         TextBox tokenTextBox = new TextBox();
         tokenTextBox.Text = "inserisci il codice";
+        tokenTextBox.Name = "tokenTextBox";
         tokenTextBox.Location = new Point(80, 175);
         tokenTextBox.Size = new Size(180, 50);
-        tokenTextBox.Font = new Font("Consolas", 10);
+        tokenTextBox.Font = new Font("Consolas", 9);
         this.Controls.Add(tokenTextBox);
 
         //create button to start the server
         Button startButton = new Button();
         startButton.Text = "AVVIA IL SERVER";
         startButton.Location = new Point(70, 235);
-        startButton.Font = new Font("Tahoma", 13);
+        startButton.Font = new Font("Tahoma", 11, FontStyle.Bold);
         startButton.Size = new Size(200, 50);
-        startButton.BackColor = Color.DarkOrange;
-        startButton.ForeColor= Color.White;
+        startButton.BackColor = Color.SkyBlue;
         startButton.UseCompatibleTextRendering = true;
         this.Controls.Add(startButton);
 
         Label statusLabel = new Label();
+        statusLabel.Name = "statusLabel";
         statusLabel.Location = new Point(70, 290);
-        statusLabel.Text = "lol";
+        statusLabel.Size = new Size(200, 50);
         statusLabel.Font = new Font("Consolas", 9);
+        statusLabel.TextAlign = ContentAlignment.MiddleCenter;
         this.Controls.Add(statusLabel);
 
         // click handler
@@ -76,35 +80,32 @@ public class Form1 : Form
     {
         Label statusLabel = this.Controls.Find("statusLabel" , true).FirstOrDefault() as Label;
         TextBox tokenBox = this.Controls.Find("tokenTextBox" , true).FirstOrDefault() as TextBox;
+        string githubToken = tokenBox.Text;
+        string username = "";
+        string repo = "";
+        string eventType = "";
 
-        try
-        {
-            string githubToken = tokenBox.Text;
-            string username = "";
-            string repo = "";
-            string eventType = "";
+        if (String.IsNullOrEmpty(githubToken)){
+            statusLabel.Text = "Code can't be empty.";
+            statusLabel.ForeColor = Color.DarkRed;
+            return;
+        }
 
-            var content = new FormUrlEncodedContent(new[]
-            {
-                new KeyValuePair<string, string>("event_type", eventType),
-            });
+        var content = new FormUrlEncodedContent(new[]{new KeyValuePair<string, string>("event_type", eventType)});
 
-            httpClient.DefaultRequestHeaders.Clear();
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", githubToken); 
+        httpClient.DefaultRequestHeaders.Clear();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", githubToken); 
 
-            var httpResponseMessage = await httpClient.PostAsync(new Uri($"https://api.github.com/repos/{username}/{repo}/dispatches"), content);
-            string resp = await httpResponseMessage.Content.ReadAsStringAsync();
+        var httpResponseMessage = await httpClient.PostAsync(new Uri($"https://api.github.com/repos/{username}/{repo}/dispatches"), content);
 
+        if (httpResponseMessage.ToString().Contains("StatusCode: 204")){
             statusLabel.Text = "Operation successful.";
             statusLabel.ForeColor = Color.DarkGreen;
-
         }
-        catch(Exception ex)
-        {
-            statusLabel.Text = $"An error has occurred.";
+        else{
+            statusLabel.Text = "Something went wrong.";
             statusLabel.ForeColor = Color.DarkRed;
         }
-        
     }
 
     public string RandomPhrase( string[] phrases )
