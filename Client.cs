34b47b488a47582116bf77ac9ae64936a93b0ea4
Eugenio Guarino <eugenio-97@live.it>
2024-03-24 22:48:25 +0100

    feat: update with new repo

--- Client.cs
+++ Client.cs
@@ -77,7 +77,7 @@ public class Client : Form
         TextBox tokenBox = this.Controls.Find("tokenTextBox", true).FirstOrDefault() as TextBox;
         string githubToken = tokenBox.Text;
         string username = "eugenio-guarino";
-        string repo = "gcp-minecraft-server";
+        string repo = "aria-minecraft-server";
         string result = " ";
 
         if (String.IsNullOrEmpty(githubToken))
